ee5b1034f01021a09900c8ac9de46ad749402f4f
gohomewho <ga676005@hotmail.com.tw>
2021-03-08 15:43:28 +0800

    Add dragging effect

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,58 +1,113 @@
 import addGlobalEventListener from './utils/addGlobalEventListener.js'
 
 export default function setup() {
-  addGlobalEventListener('mousedown', '[data-draggable]', e => {
+  addGlobalEventListener('mousedown', '[data-draggable]', (e) => {
     const selectedItem = e.target
     const itemClone = selectedItem.cloneNode(true)
-    const originalLane = selectedItem.closest('.tasks')
-    const offset = setupDragItems(selectedItem, itemClone, e)
+    const ghost = selectedItem.cloneNode(true)
+    const originalLane = selectedItem.closest('[data-drop-zone]')
+    const offset = setupDragItems(selectedItem, itemClone, ghost, e)
 
-    setupDragEvents(selectedItem, itemClone, offset, originalLane)
-
+    setupDragEvents(selectedItem, itemClone, offset, originalLane, ghost)
   })
 }
 
-function setupDragEvents(selectedItem, itemClone, offset, originalLane) {
+/**
+ * 拖曳和放開
+ * @param {Element} selectedItem 
+ * @param {Element} itemClone 
+ * @param {Object} offset 
+ * @param {Element} originalLane 
+ * @param {Element} ghost 
+ */
+function setupDragEvents(selectedItem, itemClone, offset, originalLane, ghost) {
   const mousemoveFunction = (e) => {
     positionClone(itemClone, e, offset)
+    const dropZone = e.target.closest('[data-drop-zone]')
+    if (dropZone == null) return
+    const closestChild = [...dropZone.children].find(child => {
+      const rect = child.getBoundingClientRect()
+      return e.clientY < (rect.top + rect.height / 2)
+    })
+    if (closestChild) {
+      dropZone.insertBefore(ghost, closestChild)
+    } else {
+      dropZone.appendChild(ghost)
+    }
   }
 
   document.addEventListener('mousemove', mousemoveFunction)
 
   document.addEventListener('mouseup', (e) => {
     document.removeEventListener('mousemove', mousemoveFunction)
-    itemClone.remove()
 
-    if (e.target.closest('.tasks')) {
-      e.target.closest('.tasks').appendChild(selectedItem)
-    } else {
-      originalLane.appendChild(selectedItem)
-    }
+    stopDrag(itemClone, ghost, selectedItem, originalLane, e)
 
   }, { once: true })
+  // 只執行一次，不然物件會疊加，每次滑鼠放掉時，前面點過的物件都會再次被移動
 }
 
-function setupDragItems(selectedItem, itemClone, e) {
+/**
+ * 停止拖曳時把選取的物件移動到它該去的地方
+ * @param {Element} itemClone 
+ * @param {Element} ghost 
+ * @param {Element} selectedItem 
+ * @param {Element} originalLane 物件原本的欄位
+ * @param {Event} e 
+ */
+function stopDrag(itemClone, ghost, selectedItem, originalLane, e) {
+  const dropZone = e.target.closest('[data-drop-zone]')
+
+  if (dropZone) {
+    dropZone.insertBefore(selectedItem, ghost)
+  } else {
+    originalLane.appendChild(selectedItem)
+  }
+
+  itemClone.remove()
+  ghost.remove()
+}
+
+/**
+ * 拖曳效果
+ * @param {Element} selectedItem 
+ * @param {Element} itemClone 
+ * @param {Element} ghost 
+ * @param {Event} e 
+ * @returns {Object} offset
+ */
+function setupDragItems(selectedItem, itemClone, ghost, e) {
   const originalRect = selectedItem.getBoundingClientRect()
 
-  // 滑鼠跟物件的相對位置
+  // 滑鼠跟物件左邊和上方的距離
   const offset = {
     x: e.clientX - originalRect.left,
     y: e.clientY - originalRect.top
   }
 
-  selectedItem.remove()
-
   itemClone.classList.add('dragging')
-
   // 因為dragging class的position: absolute所以要把寬度設回去
   itemClone.style.width = `${originalRect.width}px`
   document.body.append(itemClone)
   positionClone(itemClone, e, offset)
 
+  ghost.classList.add('ghost')
+  ghost.innerHTML = ''
+  // 去掉內容後把高度加回去才不會扁扁的
+  ghost.style.height = `${originalRect.height}px`
+
+  selectedItem.parentElement.insertBefore(ghost, selectedItem)
+  selectedItem.remove()
+
   return offset
 }
 
+/**
+ * 定位被選取的物件位置
+ * @param {Element} itemClone 
+ * @param {Event} event 
+ * @param {Object} offset 滑鼠點擊物件時的對物件left和top的相對距離
+ */
 function positionClone(itemClone, event, offset) {
   itemClone.style.top = `${event.clientY - offset.y}px`
   itemClone.style.left = `${event.clientX - offset.x}px`
